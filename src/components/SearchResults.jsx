fe081ffacab4839b8f72ed330752a58a79aa453e
mwyywm <hasanof.rasmus@gmail.com>
2022-01-29 23:36:47 +0100

    rough portfolio section added, needs some loading state

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,11 +1,11 @@
 import react from "react";
 import "./searchresults.css";
 
-export default function SearchResults({ data, onSelect }) {
+export default function SearchResults({ data, onClick }) {
   return (
     <div className="results-container">
       {data.map((result) => (
-        <div key={result.id} className="coin-search-result">
+        <div key={result.name} className="coin-search-result" onClick={onClick}>
           <img src={result.thumb} alt={result.name} />
           <p>{result.name}</p>
         </div>
--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -9,6 +9,7 @@ function Tracker() {
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedSearchTerm = useDebounce(searchTerm, 450);
   const [results, setResults] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
 
   function handleInputChange(event) {
     event.preventDefault();
@@ -19,6 +20,16 @@ function Tracker() {
       setSearchTerm(event.target.value);
       console.log("event.target.value:", event.target.value);
     }
+  }
+  function handleClick(event) {
+    if (event.target.tagName === "IMG") {
+      console.log(event.target.alt);
+      portfolio.push(event.target.alt);
+    } else {
+      console.log(event.target.innerText);
+      portfolio.push(event.target.innerText);
+    }
+    console.log("portfolio", portfolio);
   }
 
   useEffect(() => {
@@ -34,7 +45,6 @@ function Tracker() {
     } else {
     }
   }, [debouncedSearchTerm]);
-  console.log(results);
   return (
     <section className="tracker">
       <h1>Portfolio tracker</h1>
@@ -47,7 +57,20 @@ function Tracker() {
         <p>Add coin:</p>
         <div className="search">
           <SearchInput onInput={handleInputChange} />
-          <SearchResults data={results} />
+          <SearchResults data={results} onClick={handleClick} />
+        </div>
+        <div>
+          <p>Portfolio:</p>
+          <ul className="portfolio">
+            {portfolio.map((coin) => (
+              <li key={coin}>
+                <p>{coin}</p>
+                <p>Holdings: localstorage</p>
+                <button>edit holdings</button>
+                <button style={{ backgroundColor: "red" }}>X</button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
