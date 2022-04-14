ffc83af19840dce20d55aa7e0d30a8482039be70
mwyywm <hasanof.rasmus@gmail.com>
2022-04-14 21:06:56 +0200

    Navbar css changes

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -2,8 +2,8 @@ export default function formatNumber(num, fixedNum) {
   // formats number to string with commas and {fixedNum} amount of decimal places
   // ex: formatNumber(1234567.8944, 2) => 1,234,567.8944 || 1,234,567.89
   if (fixedNum && typeof fixedNum === "number") {
-    return new Intl.NumberFormat("en-US")?.format(num)?.floor(fixedNum);
+    return new Intl.NumberFormat("en-US").format(num?.toFixed(fixedNum));
   } else {
-    return new Intl.NumberFormat("en-US")?.format(num);
+    return new Intl.NumberFormat("en-US").format(num);
   }
 }
