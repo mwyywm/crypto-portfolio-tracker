5a5746246e22173ac6c5cff17ff58d7c2003f691
mwyywm <hasanof.rasmus@gmail.com>
2022-04-18 14:54:29 +0200

    marketCard component: Added conditional rendering depending on what the title is

--- src/components/MarketInfo.jsx
+++ src/components/MarketInfo.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./marketInfo.css";
 import useSWR from "swr";
 import fetcher from "../utils/fetcher";
 import MarketCard from "./marketCard";
-// https://api.coingecko.com/api/v3/search/trending
-// https://api.coingecko.com/api/v3/global
-// we need to fetch from the two API endpoints and then display the data.
+
+const formatBigNum = Intl.NumberFormat("en", { notation: "compact" });
 
 export default function MarketInfo() {
   const { data, error } = useSWR(
@@ -20,31 +19,34 @@ export default function MarketInfo() {
   if (!data && !trendingData) return <div>loading...</div>;
   // market_cap_change_percentage_24h_usd, market_cap_percentage.btc, market_cap_percentage.eth, total_market_cap.usd, total_volume.usd
   // can also show last updated date?
-  console.log(trendingData.coins);
 
+  console.log("data", data);
   const cardObjects = {
     globalmktcap: {
       title: "Global Market Cap",
-      value: data?.data.total_market_cap?.usd,
-      change: data?.data.market_cap_change_percentage_24h_usd,
+      value: formatBigNum.format(data?.data.total_market_cap?.usd),
+      change: data?.data.market_cap_change_percentage_24h_usd?.toFixed(2) + "%",
     },
     globalvolume: {
-      title: "Global 24h Volume",
-      value: data?.data.total_volume?.usd,
+      title: "Global Volume (24h)",
+      value: formatBigNum.format(data?.data.total_volume?.usd),
     },
     btcDominance: {
       title: "BTC Dominance",
-      value: data?.data.market_cap_percentage?.btc,
+      value: data?.data.market_cap_percentage?.btc?.toFixed(2) + "%",
     },
     ethDominance: {
       title: "ETH Dominance",
-      value: data?.data.market_cap_percentage?.eth,
+      value: data?.data.market_cap_percentage?.eth?.toFixed(2) + "%",
     },
     trending: {
       title: "Trending",
       value: trendingData.coins[0].item.name,
+      valueImg: trendingData.coins[0].item.thumb,
       value2: trendingData.coins[1].item.name,
+      valueImg2: trendingData.coins[1].item.thumb,
       value3: trendingData.coins[2].item.name,
+      valueImg3: trendingData.coins[2].item.thumb,
     },
   };
   return (
--- src/components/marketCard.jsx
+++ src/components/marketCard.jsx
@@ -1,14 +1,62 @@
-import React, { Component } from "react";
-import "./marketCard.css";
+import React from "react";
+import "./MarketCard.css";
 
 export default function MarketCard({ card }) {
-  return (
-    <div className="market-card">
-      <p className="market-card-title">{card[1].title}</p>
-      <div className="market-card-text">
-        <p>{card[1].value}</p>
-        <p>{card[1].change}</p>
+  console.log(card[1]);
+  if (card[1].title === "Trending")
+    return (
+      <div className="market-card">
+        <p className="market-card-title">{card[1].title}</p>
+        <ul className="market-card-trending">
+          <li className="market-card-value">
+            {" "}
+            <img src={card[1].valueImg} alt={card[1].value} />
+            {card[1].value}
+          </li>
+          <li className="market-card-value">
+            {" "}
+            <img src={card[1].valueImg2} alt={card[1].value2} />
+            {card[1].value2}
+          </li>
+          <li className="market-card-value">
+            {" "}
+            <img src={card[1].valueImg3} alt={card[1].value3} />
+            {card[1].value3}
+          </li>
+        </ul>
       </div>
-    </div>
-  );
+    );
+  else if (
+    card[1].title === "Global Market Cap" ||
+    card[1].title === "Global Volume (24h)"
+  )
+    return (
+      <div className="market-card">
+        <p className="market-card-title">{card[1].title}</p>
+        <div className="market-card-text">
+          <p>{card[1].value}</p>
+          <p
+            className="market-card-change"
+            style={{
+              color: card[1].change > 0 ? "green" : "red",
+              paddingLeft: "5px",
+            }}
+          >
+            {card[1].change}
+          </p>
+        </div>
+      </div>
+    );
+  else if (
+    card[1].title === "BTC Dominance" ||
+    card[1].title === "ETH Dominance"
+  )
+    return (
+      <div className="market-card">
+        <p className="market-card-title">{card[1].title}</p>
+        <div className="market-card-text">
+          <p>{card[1].value}</p>
+        </div>
+      </div>
+    );
 }
