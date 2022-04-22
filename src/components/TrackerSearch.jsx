634ba7528989bdb92d7d8727164991688a8034cd
mwyywm <hasanof.rasmus@gmail.com>
2022-04-22 15:29:45 +0200

    Added downshift for keyboard navigation in search

--- src/components/TrackerSearch.jsx
+++ src/components/TrackerSearch.jsx
@@ -6,6 +6,8 @@ import useDebounce from "../hooks/useDebounce.jsx";
 import { useOnClickOutside } from "../hooks/useOnClickOutside";
 import uuid from "../utils/uuid";
 import useSWR from "swr";
+import { render } from "react-dom";
+import { useCombobox } from "downshift";
 
 export default function TrackerSearch({
   portfolio,
@@ -134,3 +136,117 @@ export default function TrackerSearch({
     </>
   );
 }
+
+export function DropdownCombobox({ portfolio, setModalContent, setShowModal }) {
+  const [inputItems, setInputItems] = useState("hi", "yo");
+  const [searchTerm, setSearchTerm] = useState(""); // value of the search input
+  const debouncedSearchTerm = useDebounce(searchTerm, 450); // search debounce
+  const [results, setResults] = useState([]); // search results
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: results,
+    onInputValueChange: ({ inputValue }) => {
+      console.log(inputValue);
+      setSearchTerm(inputValue);
+    },
+  });
+  function handleSearchClick(event) {
+    event.preventDefault();
+    // If the coin we click already exists in the portfolio, we don't want to add it again.
+    if (
+      [...portfolio].some(
+        (coin) =>
+          coin.name === event.target.alt || coin.name === event.target.innerText
+      )
+    ) {
+      return setShowError(true);
+    }
+    if (event.target.tagName === "IMG") {
+      // add to modalContent
+      setModalContent({
+        name: event.target.alt,
+        apiID: results.find((result) => result.name === event.target.alt).id,
+        image: results.find((result) => result.name === event.target.alt).large,
+        symbol:
+          results.find((result) => result.name === event.target.alt).symbol ||
+          "",
+        holdings: 0,
+        price: 0,
+        uuid: uuid(),
+      });
+      setShowError(false);
+      setShowModal(true);
+      setSearchTerm("");
+      setResults([]);
+    } else if (event.target.tagName === "P" || event.target.tagName === "LI") {
+      setModalContent({
+        name: event.target.innerText,
+        apiID: results.find((result) => result.name === event.target.innerText)
+          .id,
+        image: results.find((result) => result.name === event.target.innerText)
+          .large,
+        symbol:
+          results.find((result) => result.name === event.target.innerText)
+            .symbol || "",
+        holdings: 0,
+        price: 0,
+        uuid: uuid(),
+      });
+      setShowModal(true);
+      // trigger modal and clear search + results
+      setShowError(false);
+      setSearchTerm("");
+      setResults([]);
+    }
+  }
+
+  const { data: searchData, error: searchError } = useSWR(
+    debouncedSearchTerm.length > 1
+      ? `https://api.coingecko.com/api/v3/search?query=${debouncedSearchTerm}`
+      : null,
+    {
+      revalidateOnFocus: false,
+      onSuccess: (searchData) => {
+        setResults(searchData.coins.slice(0, 30));
+      },
+    }
+  );
+  return (
+    <>
+      <div {...getComboboxProps()}>
+        <input {...getInputProps()} />
+        <button
+          type="button"
+          {...getToggleButtonProps()}
+          aria-label={"toggle menu"}
+        >
+          &#8595;
+        </button>
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          results.map((result, i) => (
+            <li
+              key={result.name}
+              className="coin-search-result"
+              onClick={handleSearchClick}
+            >
+              <img src={result.thumb} alt={result.name} />
+              <p>{result.name}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+render(<DropdownCombobox />, document.getElementById("root"));
--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -6,7 +6,7 @@ import formatNumber from "../utils/formatNumber";
 import { useOnClickOutside } from "../hooks/useOnClickOutside";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import useSWR from "swr";
-import TrackerSearch from "../components/TrackerSearch";
+import TrackerSearch, { DropdownCombobox } from "../components/TrackerSearch";
 
 function Tracker() {
   const [modalError, setModalError] = useState(false); // boolean
@@ -186,6 +186,11 @@ function Tracker() {
         </section>
         <section className="bottom-section">
           <TrackerSearch
+            setShowModal={setShowModal}
+            setModalContent={setModalContent}
+            portfolio={portfolio}
+          />
+          <DropdownCombobox
             setShowModal={setShowModal}
             setModalContent={setModalContent}
             portfolio={portfolio}
