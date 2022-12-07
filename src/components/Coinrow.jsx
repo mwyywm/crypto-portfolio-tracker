b7e5b75c28029f6ec0516ec2a81a57001736382f
mwyywm <hasanof.rasmus@gmail.com>
2022-12-07 19:25:23 +0100

    feat: table sorting

--- src/components/Coinrow.jsx
+++ src/components/Coinrow.jsx
@@ -6,84 +6,64 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-// const defaultData = [
-//   {
-//     name: "Bitcoin",
-//     price: "linsley",
-//     market_cap: 24,
-//     volume: 100000,
-//     change: 2,
-//   },
-//   {
-//     name: "tandy",
-//     price: "miller",
-//     market_cap: 40,
-//     volume: 100000,
-//     change: 2,
-//   },
-//   {
-//     name: "joe",
-//     price: "dirte",
-//     market_cap: 45,
-//     volume: 100000,
-//     change: 2,
-//   },
-// ];
 
 const columnHelper = createColumnHelper();
 
-const columns = [
-  columnHelper.accessor("name", {
-    header: () => <span>Name</span>,
-    cell: (info) => (
-      <a href={`/coin/${info.row.original.id}`}>{info.getValue()}</a>
-    ),
-  }),
-  columnHelper.accessor("current_price", {
-    header: () => <span>Price</span>,
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("market_cap", {
-    header: () => <span>Market cap</span>,
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("total_volume", {
-    header: () => <span>24h volume</span>,
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("price_change_percentage_24h", {
-    header: "24h Change",
-    cell: (info) => info.getValue(),
-  }),
-];
-
 export default function Coinrow() {
   const [page, setPage] = React.useState(1);
+  const [sorting, setSorting] = React.useState([
+    { desc: true, id: "market_cap" },
+  ]);
   const { data, error, isValidating } = useSWR(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${page}&sparkline=false`,
     {
       revalidateOnFocus: false,
-      onSuccess: (res) => {
-        let arr = [];
-        res.map((coin) => {
-          arr.push(coin);
-        });
-      },
     }
   );
+  const columns = React.useMemo(
+    () => [
+      columnHelper.accessor("name", {
+        header: () => <span>Name</span>,
+        cell: (props) => (
+          <a href={`/coin/${props.row.original.id}`}>{props.getValue()}</a>
+        ),
+      }),
+      columnHelper.accessor("current_price", {
+        header: () => <span>Price</span>,
+        cell: (props) => props.getValue(),
+      }),
+      columnHelper.accessor("market_cap", {
+        header: () => <span>Market cap</span>,
+        cell: (props) => props.getValue(),
+      }),
+      columnHelper.accessor("total_volume", {
+        header: () => <span>24h volume</span>,
+        cell: (props) => props.getValue(),
+      }),
+      columnHelper.accessor("price_change_percentage_24h", {
+        header: "24h Change",
+        cell: (props) => props.getValue(),
+      }),
+    ],
+    []
+  );
+
   const rerenderDebug = React.useReducer(() => ({}), {})[1];
 
   const table = useReactTable({
-    data: data,
+    data,
     columns,
     state: {
-      columnVisibility: {
-        id: false,
-      },
+      sorting,
     },
+    enableSorting: true,
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
   });
 
   if (isValidating || !data) {
@@ -103,16 +83,28 @@ export default function Coinrow() {
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        style={{ cursor: "pointer" }}
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+
+                        {{
+                          asc: " 🔼",
+                          desc: " 🔽",
+                        }[header.column.getIsSorted()] ?? null}
+                      </div>
+                    )}
+                  </th>
+                );
+              })}
             </tr>
           ))}
         </thead>
